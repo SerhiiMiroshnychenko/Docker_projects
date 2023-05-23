ead5d4bfbc167b3bb15495317a2db60a8e8fa970
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-05-23 17:18:23 +0300

    Calendar

---
+++ python-app/main.py
@@ -0,0 +1,10 @@
+import calendar
+
+print('Вітаю в КОЛЕНДАРі!')
+
+year = int(input('Будь ласка, введіть рік: '))
+month = int(input('Будь ласка, введіть місяць: '))
+
+print(calendar.month(year, month))
+
+print('Всього найкращого!')

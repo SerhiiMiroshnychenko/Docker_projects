70ac8767617954c0b5bf17c33c59e3dffcc569c1
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-05-27 09:29:14 +0300

    Server

--- client-server/server.py
+++ client-server/server.py
@@ -4,11 +4,12 @@ import socket
 HOST = '127.0.0.1'
 PORT = 65432
 
+print(f'Server with HOST: {HOST} and PORT: {PORT} up and listening!')
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     connection, address = s.accept()
-    print(f'Server with HOST: {HOST} and PORT: {PORT} up and listening!')
     while connection:
         print(f'Connected with {address}')
         while True:
@@ -17,5 +18,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             connection.sendall(answer)
             print(f'{data=}')
             print(f'{answer=}')
-            if answer == 'EXIT':
+            if answer == b'EXIT':
                 break

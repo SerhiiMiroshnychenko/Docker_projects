532a59742b1e4f5147d73b8bc21c4e9a9ce28040
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-05-27 09:16:54 +0300

    Client

--- client-server/client.py
+++ client-server/client.py
@@ -8,9 +8,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     while True:
         message = input('Enter your text: ')
-        if message == 'exit':
-            break
         s.sendall(message.encode('utf8'))
         data = s.recv(1024)
         answer = data.decode('utf8')
         print(f"Server's echo {answer}")
+        if message == 'EXIT':
+            break

314ada7bf72e85c546af0ee8b3d113eff6d9e064
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-05-28 09:41:57 +0300

    Ukraine and docker-compose

--- time-app/frontend/src/components/Time.vue
+++ time-app/frontend/src/components/Time.vue
@@ -29,20 +29,15 @@ export default {
 <template>
   <div class="greetings">
     <h1 class="green">{{ currentTime }}</h1>
-    <h3>Нажмите кнопку для сохранения в базе данных</h3>
-    <button @click="saveTime">Сохранить время</button>
+    <h3>Натисніть кнопку для збереження в базу даних</h3>
+    <button @click="saveTime">Зберегти час</button>
     <h3 v-if="savedTimes.length && showSavedTimes">
-      Ранее сохраненные времена:
+      Раніше збережені часи:
     </h3>
-    <div
-      v-if="showSavedTimes"
-      class="deleted-items"
-      v-for="savedTime in savedTimes"
-      :key="savedTime.id"
-    >
+    <div v-if="showSavedTimes" class="deleted-items" v-for="savedTime in savedTimes" :key="savedTime.id">
       <div class="deleted-item">{{ savedTime.time }}</div>
       <button class="btn-sm bg-red" @click="() => deleteTime(savedTime.id)">
-        Удалить
+        Видалити
       </button>
     </div>
   </div>

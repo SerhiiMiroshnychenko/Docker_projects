e61c03d636a38c8e026507c41a180eed22657cac
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-05-28 21:24:56 +0300

    hot reload for backend

--- time-app/api/index.mjs
+++ time-app/api/index.mjs
@@ -14,7 +14,7 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.get('/', (_, res) => {
-  res.send('Hello from the time saving service!')
+  res.send('Вітання від сервісу збереження часу! Слава Україні!!!')
 })
 
 app.get('/times', async (_, res) => {
